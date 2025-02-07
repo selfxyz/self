078d1464e20e9f7972a19fadb7e58e0a54103a94
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-02-07 15:30:08 +0100

    install scanner

--- app/src/components/native/QRCodeScanner.tsx
+++ app/src/components/native/QRCodeScanner.tsx
@@ -2,11 +2,14 @@ import React, { useCallback } from 'react';
 import {
   NativeSyntheticEvent,
   PixelRatio,
+  Platform,
   requireNativeComponent,
 } from 'react-native';
 
 import { RCTFragment, RCTFragmentViewManagerProps } from './RCTFragment';
-
+import {
+  ReactNativeScannerView,
+} from '@pushpendersingh/react-native-scanner';
 interface RCTQRCodeScannerViewProps extends RCTFragmentViewManagerProps {
   onQRData: (event: NativeSyntheticEvent<{ data: string }>) => void;
 }
@@ -48,19 +51,41 @@ export const QRCodeScannerView: React.FC<QRCodeScannerViewProps> = ({
     [onQRData, isMounted],
   );
 
-  return (
-    <Fragment
-      RCTFragmentViewManager={RCTQRCodeScannerViewManager}
+  const scannerRef = React.useRef(null);
+
+  const style = {
+    // converts dpi to px, provide desired height
+    height: PixelRatio.getPixelSizeForLayoutSize(800),
+    // converts dpi to px, provide desired width
+    width: PixelRatio.getPixelSizeForLayoutSize(400),
+  }
+
+  const handleBarcodeScanned = (event: NativeSyntheticEvent<{data: string, type: string}>) => {
+    const {data, type} = event?.nativeEvent;
+    console.log('Barcode / QR Code scanned:', data, type);
+  };
+
+   
+  if (Platform.OS === 'android') { 
+    // @ts-expect-error
+    return <Fragment RCTFragmentViewManager={RCTQRCodeScannerViewManager}
       fragmentComponentName={RCT_COMPONENT_NAME}
       isMounted={isMounted}
-      style={{
-        // converts dpi to px, provide desired height
-        height: PixelRatio.getPixelSizeForLayoutSize(800),
-        // converts dpi to px, provide desired width
-        width: PixelRatio.getPixelSizeForLayoutSize(400),
-      }}
+      style={style}
       onError={_onError}
       onQRData={_onQRData}
     />
-  );
+  }
+  else if (Platform.OS === 'ios') {
+    return <ReactNativeScannerView
+      ref={scannerRef}
+      style={style}
+      onQrScanned={handleBarcodeScanned}
+      pauseAfterCapture={true} // Pause the scanner after barcode / QR code is scanned
+      isActive={true} // Start / stop the scanner using this prop
+      showBox={true} // Show the box around the barcode / QR code
+    />
+  }
+  return null;
 };
+
