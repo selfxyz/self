ad47ccb883e1083ce36a09360b95978f2510e8fd
Justin Hernandez <transphorm@gmail.com>
2025-02-11 10:12:39 -0600

    save wip eslint settings

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,9 +1,48 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-  ignorePatterns: ['ios/', 'android/', 'deployments/', 'node_modules/'],
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  ignorePatterns: [
+    'ios/',
+    'android/',
+    'deployments/',
+    'node_modules/',
+    '*.js.map',
+    '*.d.ts',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'react-native/no-inline-styles': 'off',
-    'react-hooks/exhaustive-deps': 'off',
+    // React Rules
+    'react/react-in-jsx-scope': 'off', // Not needed in newer React versions
+    'react/prop-types': 'off', // We use TypeScript for prop validation
+    'react-native/no-inline-styles': 'warn', // Warning instead of off to encourage better practices
+    'react-hooks/exhaustive-deps': 'warn', // Warning to catch potential issues
+
+    // TypeScript Rules
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+
+    // General Rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'warn',
+    'no-duplicate-imports': 'error',
   },
 };
