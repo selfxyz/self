616476baa381ce45024c15a2bdb2833a14584e0a
turnoffthiscomputer <colin.remi07@gmail.com>
2025-02-11 21:56:52 +0100

    update appTypes

--- common/src/utils/appType.ts
+++ common/src/utils/appType.ts
@@ -1,20 +1,18 @@
 import { UserIdType } from "./circuits/uuid";
 
-export type CircuitName = 'disclose';
-export type Mode = 'prove_offchain' | 'prove_onchain' | 'register' | 'vc_and_disclose' | 'dsc';
+export type Mode = 'register' | 'dsc' | 'vc_and_disclose';
 
 // OpenPassportAppType
-export interface SelfAppPartial {
+export interface OpenPassportAppPartial {
   appName: string;
   scope: string;
-  websocketUrl: string;
   sessionId: string;
   userId: string;
   userIdType: UserIdType;
   devMode: boolean;
 }
 
-export interface SelfApp extends SelfAppPartial {
+export interface OpenPassportApp extends OpenPassportAppPartial {
   args: ArgumentsProveOffChain | ArgumentsProveOnChain | ArgumentsRegister | ArgumentsDisclose;
 }
 
@@ -36,11 +34,31 @@ export interface ArgumentsRegister {
 
 export interface ArgumentsDisclose {
   disclosureOptions: DisclosureOptions;
+  commitmentMerkleTreeUrl: string;
 }
 
-export interface DisclosureOptions {
-  minimumAge: { enabled: boolean; value: string };
-  nationality: { enabled: boolean; value: string };
-  excludedCountries: { enabled: boolean; value: string[] };
-  ofac: boolean;
+type DisclosureBoolKeys = 'ofac'
+type DisclosureBoolOption = {
+  enabled: boolean;
+  key: DisclosureBoolKeys
 }
+
+type DisclosureMatchKeys = 'nationality' | 'minimumAge'
+interface DisclosureMatchOption<T = DisclosureMatchKeys> {
+  enabled: boolean;
+  key: T;
+  value: string;
+}
+
+type DisclosureListKeys = 'excludedCountries'
+interface DisclosureListOption {
+  enabled: boolean;
+  key: DisclosureListKeys;
+  value: string[];
+}
+
+export type DisclosureOption = DisclosureBoolOption | DisclosureMatchOption | DisclosureListOption
+export type DisclosureAttributes = DisclosureBoolKeys | DisclosureMatchKeys | DisclosureListKeys
+export type DisclosureOptions = Array<DisclosureOption>
+
+export type GetDisclosure<T extends DisclosureAttributes> = T extends DisclosureMatchKeys ? DisclosureMatchOption : T extends DisclosureListKeys ? DisclosureListOption : DisclosureBoolOption
