0e2ac06b4e550bad8605a6de1205948da48d8a64
thomas-senechal <thomas.senechal@pm.me>
2025-02-11 23:09:28 +0100

    Fmt AttestationVerifier and

--- sdk/core/index.ts
+++ sdk/core/index.ts
@@ -2,8 +2,8 @@ import { SelfVerifierReport } from './src/SelfVerifierReport';
 import { SelfVerifier } from './src/SelfVerifier';
 import { countryCodes } from '../../common/src/constants/constants';
 import {
-  OpenPassportAttestation,
-  OpenPassportDynamicAttestation,
+  SelfAttestation,
+  // OpenPassportDynamicAttestation,
 } from '../../common/src/utils/selfAttestation';
 export {
   SelfVerifier,
