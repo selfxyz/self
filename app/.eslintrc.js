e554407649d3c49955e0375c31233dcabda62a9c
Justin Hernandez <transphorm@gmail.com>
2025-02-12 13:54:58 -0600

    add eslint perf pkg

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -6,9 +6,10 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react-perf/all',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-perf'],
   ignorePatterns: [
     'ios/',
     'android/',
