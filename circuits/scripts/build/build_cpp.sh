f4cf5b96d08bdcb13d72752d917b93f41bce6fab
0xturboblitz <florent.tavernier@gmail.com>
2025-02-19 21:07:57 -0800

    add new instance to build_cpp

--- circuits/scripts/build/build_cpp.sh
+++ circuits/scripts/build/build_cpp.sh
@@ -21,6 +21,7 @@ REGISTER_CIRCUITS=(
     "register_sha256_sha256_sha256_rsa_65537_4096:true"
     "register_sha256_sha256_sha256_rsapss_3_32_4096:false"
     "register_sha256_sha256_sha256_rsapss_65537_4096:false"
+    "register_sha384_sha384_sha384_rsapss_65537_48_2048:false"
     "register_sha384_sha384_sha384_ecdsa_brainpoolP256r1:false"
     "register_sha384_sha384_sha384_ecdsa_brainpoolP384r1:false"
     "register_sha384_sha384_sha384_ecdsa_secp384r1:false"
