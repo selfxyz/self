7d5017d2ed7f49690c7df64fc8f3c2a7179d7e40
ayman <aymanshaik1015@gmail.com>
2025-02-20 12:58:26 +0530

    chore: add circuit

--- circuits/scripts/build/build_cpp.sh
+++ circuits/scripts/build/build_cpp.sh
@@ -15,6 +15,7 @@ REGISTER_CIRCUITS=(
     "register_sha256_sha256_sha256_ecdsa_brainpoolP256r1:true"
     "register_sha256_sha256_sha256_ecdsa_brainpoolP384r1:true"
     "register_sha256_sha256_sha256_ecdsa_secp256r1:true"
+    "register_sha384_sha384_sha384_rsapss_65537_48_2048:true"
     "register_sha256_sha256_sha256_ecdsa_secp384r1:true"
     "register_sha256_sha256_sha256_rsa_3_4096:true"
     "register_sha256_sha256_sha256_rsa_65537_4096:true"
