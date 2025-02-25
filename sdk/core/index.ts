9bd2801012cfc7bf917257479d6b556c927d024e
0xturboblitz <florent.tavernier@gmail.com>
2025-02-25 13:20:50 -0700

    exporting SelfVerificationResult properly

--- sdk/core/index.ts
+++ sdk/core/index.ts
@@ -1,5 +1,6 @@
 import { SelfBackendVerifier } from './src/SelfBackendVerifier';
 import { getUserIdentifier } from './src/utils/utils';
 import { countryCodes } from '../../common/src/constants/constants';
+import { SelfVerificationResult } from '../../common/src/utils/selfAttestation';
 
-export { SelfBackendVerifier, getUserIdentifier, countryCodes };
+export { SelfBackendVerifier, getUserIdentifier, countryCodes, SelfVerificationResult };
