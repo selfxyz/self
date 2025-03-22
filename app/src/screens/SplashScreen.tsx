b94a6f64cbe05dc9e95dc57ae1aa25dac2b5071f
ehsun <ehsun.ahmadi@gmail.com>
2025-03-22 14:02:26 +0330

    yarn nice

--- app/src/screens/SplashScreen.tsx
+++ app/src/screens/SplashScreen.tsx
@@ -11,64 +11,64 @@ import { impactLight } from '../utils/haptic';
 import { isUserRegistered } from '../utils/proving/payload';
 
 const SplashScreen: React.FC = ({}) => {
-	const navigation = useNavigation();
-	const { passportData, privateKey, status } = usePassport(false);
+  const navigation = useNavigation();
+  const { passportData, privateKey, status } = usePassport(false);
 
-	const handleAnimationFinish = useCallback(() => {
-		setTimeout(async () => {
-			impactLight();
-			if (status !== 'success') {
-				return;
-			}
+  const handleAnimationFinish = useCallback(() => {
+    setTimeout(async () => {
+      impactLight();
+      if (status !== 'success') {
+        return;
+      }
 
-			if (!passportData || !privateKey) {
-				navigation.navigate('Launch');
-				return;
-			}
+      if (!passportData || !privateKey) {
+        navigation.navigate('Launch');
+        return;
+      }
 
-			const isRegistered = await isUserRegistered(passportData, privateKey);
-			console.log('User is registered:', isRegistered);
-			if (isRegistered) {
-				console.log('Passport is registered already. Skipping to HomeScreen');
-				navigation.navigate('Home');
-				return;
-			}
-			// Currently, we dont check isPassportNullified(passportData);
-			// This could lead to AccountRecoveryChoice just like in LoadingScreen
-			// But it looks better right now to keep the LaunchScreen flow
-			// In case user wants to try with another passport.
-			// Long term, we could also show a modal instead that prompts the user to recover or scan a new passport.
+      const isRegistered = await isUserRegistered(passportData, privateKey);
+      console.log('User is registered:', isRegistered);
+      if (isRegistered) {
+        console.log('Passport is registered already. Skipping to HomeScreen');
+        navigation.navigate('Home');
+        return;
+      }
+      // Currently, we dont check isPassportNullified(passportData);
+      // This could lead to AccountRecoveryChoice just like in LoadingScreen
+      // But it looks better right now to keep the LaunchScreen flow
+      // In case user wants to try with another passport.
+      // Long term, we could also show a modal instead that prompts the user to recover or scan a new passport.
 
-			// Rest of the time, keep the LaunchScreen flow
-			navigation.navigate('Launch');
-		}, 1000);
-	}, [navigation, passportData, privateKey, status]);
+      // Rest of the time, keep the LaunchScreen flow
+      navigation.navigate('Launch');
+    }, 1000);
+  }, [navigation, passportData, privateKey, status]);
 
-	return (
-		<LottieView
-			autoPlay
-			loop={false}
-			source={splashAnimation}
-			style={styles.animation}
-			onAnimationFinish={handleAnimationFinish}
-			resizeMode="cover"
-			cacheComposition={true}
-			renderMode="HARDWARE"
-		/>
-	);
+  return (
+    <LottieView
+      autoPlay
+      loop={false}
+      source={splashAnimation}
+      style={styles.animation}
+      onAnimationFinish={handleAnimationFinish}
+      resizeMode="cover"
+      cacheComposition={true}
+      renderMode="HARDWARE"
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-	animation: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
-		height: '100%',
-		width: '100%',
-		backgroundColor: black,
-	},
+  animation: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: '100%',
+    width: '100%',
+    backgroundColor: black,
+  },
 });
 
 export default SplashScreen;
