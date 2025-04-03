e4e456382323e501443c3c948e5ce003ae47cc1c
turnoffthiscomputer <colin.remi07@gmail.com>
2025-04-03 09:58:48 +0800

    add self app builder to core sdk

--- sdk/core/index.ts
+++ sdk/core/index.ts
@@ -1,7 +1,7 @@
 import { SelfBackendVerifier } from './src/SelfBackendVerifier';
 import { getUserIdentifier } from './src/utils/utils';
 import { countryCodes } from '../../common/src/constants/constants';
-import { SelfApp, getUniversalLink } from '../../common/src/utils/appType';
+import { SelfApp, getUniversalLink, SelfAppBuilder } from '../../common/src/utils/appType';
 import { countries } from '../../common/src/constants/countries';
 import { hashEndpointWithScope } from '../../common/src/utils/scope';
 
@@ -13,4 +13,5 @@ export {
   getUniversalLink,
   countries,
   hashEndpointWithScope,
+  SelfAppBuilder,
 };
