9c7d9531a87679428a61573bb95985630b7d3ddd
turnoffthiscomputer <colin.remi07@gmail.com>
2025-04-04 22:36:52 +0800

    fix sdk import

--- sdk/core/index.ts
+++ sdk/core/index.ts
@@ -4,7 +4,7 @@ import { countryCodes } from '../../common/src/constants/constants';
 import { SelfApp, getUniversalLink, SelfAppBuilder } from '../../common/src/utils/appType';
 import { countries } from '../../common/src/constants/countries';
 import { hashEndpointWithScope } from '../../common/src/utils/scope';
-import { getPackedForbiddenCountries } from '@common/utils/contracts/forbiddenCountries';
+import { getPackedForbiddenCountries } from '../../common/src/utils/contracts/forbiddenCountries';
 
 export {
   SelfBackendVerifier,
