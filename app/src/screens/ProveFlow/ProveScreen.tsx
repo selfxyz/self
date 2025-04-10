7adef705da6aeec502dfadcc607561d046b7f263
ehsun <ehsun.ahmadi@gmail.com>
2025-04-10 18:24:59 +0330

    undo whitespace changes in ProveScreen

--- app/src/screens/ProveFlow/ProveScreen.tsx
+++ app/src/screens/ProveFlow/ProveScreen.tsx
@@ -1,16 +1,16 @@
 import React, {
-	useCallback,
-	useEffect,
-	useMemo,
-	useRef,
-	useState,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
 } from 'react';
 import {
-	LayoutChangeEvent,
-	NativeScrollEvent,
-	NativeSyntheticEvent,
-	ScrollView,
-	StyleSheet,
+  LayoutChangeEvent,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  ScrollView,
+  StyleSheet,
 } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
@@ -29,148 +29,148 @@ import { useApp } from '../../stores/appProvider';
 import { useAuth } from '../../stores/authProvider';
 import { usePassport } from '../../stores/passportDataProvider';
 import {
-	ProofStatusEnum,
-	globalSetDisclosureStatus,
-	useProofInfo,
+  ProofStatusEnum,
+  globalSetDisclosureStatus,
+  useProofInfo,
 } from '../../stores/proofProvider';
 import { black, slate300, white } from '../../utils/colors';
 import { buttonTap } from '../../utils/haptic';
 import {
-	isUserRegistered,
-	sendVcAndDisclosePayload,
+  isUserRegistered,
+  sendVcAndDisclosePayload,
 } from '../../utils/proving/payload';
 
 const ProveScreen: React.FC = () => {
-	const { navigate } = useNavigation();
+  const { navigate } = useNavigation();
 	const { passportData, privateKey, status: passportStatus } = usePassport();
 	const { loginWithBiometrics } = useAuth();
-	const { selectedApp, resetProof, cleanSelfApp } = useProofInfo();
-	const { handleProofResult } = useApp();
-	const selectedAppRef = useRef(selectedApp);
-	const isProcessing = useRef(false);
+  const { selectedApp, resetProof, cleanSelfApp } = useProofInfo();
+  const { handleProofResult } = useApp();
+  const selectedAppRef = useRef(selectedApp);
+  const isProcessing = useRef(false);
 
-	const [hasScrolledToBottom, setHasScrolledToBottom] = useState(false);
-	const [scrollViewContentHeight, setScrollViewContentHeight] = useState(0);
-	const [scrollViewHeight, setScrollViewHeight] = useState(0);
-	const scrollViewRef = useRef<ScrollView>(null);
+  const [hasScrolledToBottom, setHasScrolledToBottom] = useState(false);
+  const [scrollViewContentHeight, setScrollViewContentHeight] = useState(0);
+  const [scrollViewHeight, setScrollViewHeight] = useState(0);
+  const scrollViewRef = useRef<ScrollView>(null);
 
-	const isContentShorterThanScrollView = useMemo(
-		() => scrollViewContentHeight <= scrollViewHeight,
-		[scrollViewContentHeight, scrollViewHeight],
-	);
+  const isContentShorterThanScrollView = useMemo(
+    () => scrollViewContentHeight <= scrollViewHeight,
+    [scrollViewContentHeight, scrollViewHeight],
+  );
 
-	/**
-	 * Whenever the relationship between content height vs. scroll view height changes,
-	 * reset (or enable) the button state accordingly.
-	 */
-	useEffect(() => {
-		if (isContentShorterThanScrollView) {
-			setHasScrolledToBottom(true);
-		} else {
-			setHasScrolledToBottom(false);
-		}
-	}, [isContentShorterThanScrollView]);
+  /**
+   * Whenever the relationship between content height vs. scroll view height changes,
+   * reset (or enable) the button state accordingly.
+   */
+  useEffect(() => {
+    if (isContentShorterThanScrollView) {
+      setHasScrolledToBottom(true);
+    } else {
+      setHasScrolledToBottom(false);
+    }
+  }, [isContentShorterThanScrollView]);
 
-	useEffect(() => {
-		if (
-			!selectedApp ||
-			selectedAppRef.current?.sessionId === selectedApp.sessionId
-		) {
-			return; // Avoid unnecessary updates
-		}
-		selectedAppRef.current = selectedApp;
-		console.log('[ProveScreen] Selected app updated:', selectedApp);
-	}, [selectedApp]);
+  useEffect(() => {
+    if (
+      !selectedApp ||
+      selectedAppRef.current?.sessionId === selectedApp.sessionId
+    ) {
+      return; // Avoid unnecessary updates
+    }
+    selectedAppRef.current = selectedApp;
+    console.log('[ProveScreen] Selected app updated:', selectedApp);
+  }, [selectedApp]);
 
-	const disclosureOptions = useMemo(() => {
-		return (selectedApp?.disclosures as SelfAppDisclosureConfig) || [];
-	}, [selectedApp?.disclosures]);
+  const disclosureOptions = useMemo(() => {
+    return (selectedApp?.disclosures as SelfAppDisclosureConfig) || [];
+  }, [selectedApp?.disclosures]);
 
-	// Format the logo source based on whether it's a URL or base64 string
-	const logoSource = useMemo(() => {
-		if (!selectedApp?.logoBase64) {
-			return null;
-		}
+  // Format the logo source based on whether it's a URL or base64 string
+  const logoSource = useMemo(() => {
+    if (!selectedApp?.logoBase64) {
+      return null;
+    }
 
-		// Check if the logo is already a URL
-		if (
-			selectedApp.logoBase64.startsWith('http://') ||
-			selectedApp.logoBase64.startsWith('https://')
-		) {
-			return { uri: selectedApp.logoBase64 };
-		}
+    // Check if the logo is already a URL
+    if (
+      selectedApp.logoBase64.startsWith('http://') ||
+      selectedApp.logoBase64.startsWith('https://')
+    ) {
+      return { uri: selectedApp.logoBase64 };
+    }
 
-		// Otherwise handle as base64 as before
-		const base64String = selectedApp.logoBase64.startsWith('data:image')
-			? selectedApp.logoBase64
-			: `data:image/png;base64,${selectedApp.logoBase64}`;
-		return { uri: base64String };
-	}, [selectedApp?.logoBase64]);
+    // Otherwise handle as base64 as before
+    const base64String = selectedApp.logoBase64.startsWith('data:image')
+      ? selectedApp.logoBase64
+      : `data:image/png;base64,${selectedApp.logoBase64}`;
+    return { uri: base64String };
+  }, [selectedApp?.logoBase64]);
 
-	const url = useMemo(() => {
-		if (!selectedApp?.endpoint) {
-			return null;
-		}
-		return formatEndpoint(selectedApp.endpoint);
-	}, [selectedApp?.endpoint]);
+  const url = useMemo(() => {
+    if (!selectedApp?.endpoint) {
+      return null;
+    }
+    return formatEndpoint(selectedApp.endpoint);
+  }, [selectedApp?.endpoint]);
 
 	const onVerify = useCallback(async () => {
 		if (passportStatus !== 'success' || isProcessing.current) {
-			return;
-		}
-		isProcessing.current = true;
+        return;
+      }
+      isProcessing.current = true;
 
 		await loginWithBiometrics();
 
-		resetProof();
-		buttonTap();
-		const currentApp = selectedAppRef.current;
+      resetProof();
+      buttonTap();
+      const currentApp = selectedAppRef.current;
 
-		try {
-			let timeToNavigateToStatusScreen: NodeJS.Timeout;
+      try {
+        let timeToNavigateToStatusScreen: NodeJS.Timeout;
 
-			timeToNavigateToStatusScreen = setTimeout(() => {
-				navigate('ProofRequestStatusScreen');
-			}, 200);
+        timeToNavigateToStatusScreen = setTimeout(() => {
+          navigate('ProofRequestStatusScreen');
+        }, 200);
 
 			if (!passportData || !privateKey) {
-				console.log('No passport data or secret');
-				globalSetDisclosureStatus?.(ProofStatusEnum.ERROR);
-				setTimeout(() => {
-					navigate('PassportDataNotFound');
-				}, 3000);
-				return;
-			}
+          console.log('No passport data or secret');
+          globalSetDisclosureStatus?.(ProofStatusEnum.ERROR);
+          setTimeout(() => {
+            navigate('PassportDataNotFound');
+          }, 3000);
+          return;
+        }
 
 			const isRegistered = await isUserRegistered(passportData, privateKey);
-			console.log('isRegistered', isRegistered);
+        console.log('isRegistered', isRegistered);
 
-			if (!isRegistered) {
-				clearTimeout(timeToNavigateToStatusScreen);
-				console.log(
-					'User is not registered, sending to ConfirmBelongingScreen',
-				);
-				navigate('ConfirmBelongingScreen');
-				cleanSelfApp();
-				return;
-			}
+        if (!isRegistered) {
+          clearTimeout(timeToNavigateToStatusScreen);
+          console.log(
+            'User is not registered, sending to ConfirmBelongingScreen',
+          );
+          navigate('ConfirmBelongingScreen');
+          cleanSelfApp();
+          return;
+        }
 
-			console.log('currentApp', currentApp);
-			const status = await sendVcAndDisclosePayload(
+        console.log('currentApp', currentApp);
+        const status = await sendVcAndDisclosePayload(
 				privateKey,
-				passportData,
-				currentApp,
-			);
-			handleProofResult(
-				currentApp.sessionId,
-				status === ProofStatusEnum.SUCCESS,
-			);
-		} catch (e) {
-			console.log('Error in verification process');
-			globalSetDisclosureStatus?.(ProofStatusEnum.ERROR);
-		} finally {
-			isProcessing.current = false;
-		}
+          passportData,
+          currentApp,
+        );
+        handleProofResult(
+          currentApp.sessionId,
+          status === ProofStatusEnum.SUCCESS,
+        );
+      } catch (e) {
+        console.log('Error in verification process');
+        globalSetDisclosureStatus?.(ProofStatusEnum.ERROR);
+      } finally {
+        isProcessing.current = false;
+      }
 	}, [
 		navigate,
 		resetProof,
@@ -182,121 +182,121 @@ const ProveScreen: React.FC = () => {
 		handleProofResult,
 	]);
 
-	const handleScroll = useCallback(
-		(event: NativeSyntheticEvent<NativeScrollEvent>) => {
-			if (hasScrolledToBottom || isContentShorterThanScrollView) {
-				return;
-			}
-			const { layoutMeasurement, contentOffset, contentSize } =
-				event.nativeEvent;
-			const paddingToBottom = 10;
-			const isCloseToBottom =
-				layoutMeasurement.height + contentOffset.y >=
-				contentSize.height - paddingToBottom;
-			if (isCloseToBottom && !hasScrolledToBottom) {
-				setHasScrolledToBottom(true);
-				buttonTap();
-			}
-		},
-		[hasScrolledToBottom, isContentShorterThanScrollView],
-	);
+  const handleScroll = useCallback(
+    (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+      if (hasScrolledToBottom || isContentShorterThanScrollView) {
+        return;
+      }
+      const { layoutMeasurement, contentOffset, contentSize } =
+        event.nativeEvent;
+      const paddingToBottom = 10;
+      const isCloseToBottom =
+        layoutMeasurement.height + contentOffset.y >=
+        contentSize.height - paddingToBottom;
+      if (isCloseToBottom && !hasScrolledToBottom) {
+        setHasScrolledToBottom(true);
+        buttonTap();
+      }
+    },
+    [hasScrolledToBottom, isContentShorterThanScrollView],
+  );
 
-	const handleContentSizeChange = useCallback(
-		(contentWidth: number, contentHeight: number) => {
-			setScrollViewContentHeight(contentHeight);
-		},
-		[],
-	);
+  const handleContentSizeChange = useCallback(
+    (contentWidth: number, contentHeight: number) => {
+      setScrollViewContentHeight(contentHeight);
+    },
+    [],
+  );
 
-	const handleScrollViewLayout = useCallback((event: LayoutChangeEvent) => {
-		setScrollViewHeight(event.nativeEvent.layout.height);
-	}, []);
+  const handleScrollViewLayout = useCallback((event: LayoutChangeEvent) => {
+    setScrollViewHeight(event.nativeEvent.layout.height);
+  }, []);
 
-	return (
-		<ExpandableBottomLayout.Layout flex={1} backgroundColor={black}>
-			<ExpandableBottomLayout.TopSection backgroundColor={black}>
-				<YStack alignItems="center">
-					{!selectedApp.sessionId ? (
-						<LottieView
-							source={miscAnimation}
-							autoPlay
-							loop
-							resizeMode="cover"
-							cacheComposition={true}
-							renderMode="HARDWARE"
-							style={styles.animation}
-							speed={1}
-							progress={0}
-						/>
-					) : (
-						<YStack alignItems="center" justifyContent="center">
-							{logoSource && (
-								<Image
-									mb={20}
-									source={logoSource}
-									width={100}
-									height={100}
-									objectFit="contain"
-								/>
-							)}
-							<BodyText fontSize={12} color={slate300} mb={20}>
-								{url}
-							</BodyText>
-							<BodyText fontSize={24} color={slate300} textAlign="center">
-								<Text color={white}>{selectedApp.appName}</Text> is requesting
-								that you prove the following information:
-							</BodyText>
-						</YStack>
-					)}
-				</YStack>
-			</ExpandableBottomLayout.TopSection>
-			<ExpandableBottomLayout.BottomSection
-				paddingBottom={20}
-				backgroundColor={white}
-				maxHeight={'55%'}
-			>
-				<ScrollView
-					ref={scrollViewRef}
-					onScroll={handleScroll}
-					scrollEventThrottle={16}
-					onContentSizeChange={handleContentSizeChange}
-					onLayout={handleScrollViewLayout}
-				>
-					<Disclosures disclosures={disclosureOptions} />
-					<View marginTop={20}>
-						<Caption
-							textAlign="center"
-							size="small"
-							marginBottom={20}
-							marginTop={10}
-							borderRadius={4}
-							paddingBottom={20}
-						>
-							Self will confirm that these details are accurate and none of your
-							confidential info will be revealed to {selectedApp.appName}
-						</Caption>
-					</View>
-				</ScrollView>
-				<HeldPrimaryButton
-					onPress={onVerify}
-					disabled={!selectedApp.sessionId || !hasScrolledToBottom}
-				>
-					{hasScrolledToBottom
-						? 'Hold To Verify'
-						: 'Please read all disclosures'}
-				</HeldPrimaryButton>
-			</ExpandableBottomLayout.BottomSection>
-		</ExpandableBottomLayout.Layout>
-	);
+  return (
+    <ExpandableBottomLayout.Layout flex={1} backgroundColor={black}>
+      <ExpandableBottomLayout.TopSection backgroundColor={black}>
+        <YStack alignItems="center">
+          {!selectedApp.sessionId ? (
+            <LottieView
+              source={miscAnimation}
+              autoPlay
+              loop
+              resizeMode="cover"
+              cacheComposition={true}
+              renderMode="HARDWARE"
+              style={styles.animation}
+              speed={1}
+              progress={0}
+            />
+          ) : (
+            <YStack alignItems="center" justifyContent="center">
+              {logoSource && (
+                <Image
+                  mb={20}
+                  source={logoSource}
+                  width={100}
+                  height={100}
+                  objectFit="contain"
+                />
+              )}
+              <BodyText fontSize={12} color={slate300} mb={20}>
+                {url}
+              </BodyText>
+              <BodyText fontSize={24} color={slate300} textAlign="center">
+                <Text color={white}>{selectedApp.appName}</Text> is requesting
+                that you prove the following information:
+              </BodyText>
+            </YStack>
+          )}
+        </YStack>
+      </ExpandableBottomLayout.TopSection>
+      <ExpandableBottomLayout.BottomSection
+        paddingBottom={20}
+        backgroundColor={white}
+        maxHeight={'55%'}
+      >
+        <ScrollView
+          ref={scrollViewRef}
+          onScroll={handleScroll}
+          scrollEventThrottle={16}
+          onContentSizeChange={handleContentSizeChange}
+          onLayout={handleScrollViewLayout}
+        >
+          <Disclosures disclosures={disclosureOptions} />
+          <View marginTop={20}>
+            <Caption
+              textAlign="center"
+              size="small"
+              marginBottom={20}
+              marginTop={10}
+              borderRadius={4}
+              paddingBottom={20}
+            >
+              Self will confirm that these details are accurate and none of your
+              confidential info will be revealed to {selectedApp.appName}
+            </Caption>
+          </View>
+        </ScrollView>
+        <HeldPrimaryButton
+          onPress={onVerify}
+          disabled={!selectedApp.sessionId || !hasScrolledToBottom}
+        >
+          {hasScrolledToBottom
+            ? 'Hold To Verify'
+            : 'Please read all disclosures'}
+        </HeldPrimaryButton>
+      </ExpandableBottomLayout.BottomSection>
+    </ExpandableBottomLayout.Layout>
+  );
 };
 
 export default ProveScreen;
 
 const styles = StyleSheet.create({
-	animation: {
-		top: 0,
-		width: 200,
-		height: 200,
-		transform: [{ scale: 2 }, { translateY: -20 }],
-	},
+  animation: {
+    top: 0,
+    width: 200,
+    height: 200,
+    transform: [{ scale: 2 }, { translateY: -20 }],
+  },
 });
