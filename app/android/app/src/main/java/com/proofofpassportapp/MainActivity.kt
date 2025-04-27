384f70051e96ab26446a434c152bc7218d36a2c9
motemotech <i.am.nicoshark@gmail.com>
2025-04-28 01:14:46 +0900

    change place where we ask push notification

--- app/android/app/src/main/java/com/proofofpassportapp/MainActivity.kt
+++ app/android/app/src/main/java/com/proofofpassportapp/MainActivity.kt
@@ -4,11 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.content.pm.ActivityInfo
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -16,8 +11,6 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 import io.tradle.nfc.RNPassportReaderModule
 
 class MainActivity : ReactActivity() {
-  private val NOTIFICATION_PERMISSION_CODE = 100
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -41,42 +34,5 @@ class MainActivity : ReactActivity() {
     super.onCreate(savedInstanceState)
     // Lock to portrait orientation
     requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-    
-    // Request notification permission for Android 13+
-    requestNotificationPermissionIfNeeded()
-  }
-  
-  private fun requestNotificationPermissionIfNeeded() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-      Log.d("PERMISSION", "Checking notification permission for Android 13+")
-      val permission = Manifest.permission.POST_NOTIFICATIONS
-      
-      if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-        Log.d("PERMISSION", "Requesting POST_NOTIFICATIONS permission")
-        ActivityCompat.requestPermissions(
-          this,
-          arrayOf(permission),
-          NOTIFICATION_PERMISSION_CODE
-        )
-      } else {
-        Log.d("PERMISSION", "POST_NOTIFICATIONS permission already granted")
-      }
-    }
-  }
-  
-  override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<out String>,
-    grantResults: IntArray
-  ) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    
-    if (requestCode == NOTIFICATION_PERMISSION_CODE) {
-      if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        Log.d("PERMISSION", "POST_NOTIFICATIONS permission granted by user")
-      } else {
-        Log.d("PERMISSION", "POST_NOTIFICATIONS permission denied by user")
-      }
-    }
   }
 }
