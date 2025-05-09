e1fa1aebe7848abebb01eeb6301d23c712074cf5
Justin Hernandez <transphorm@gmail.com>
2025-05-08 19:01:18 -0500

    save working jest import test

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community)/)',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleNameMapper: {
+    '^@env$': '<rootDir>/tests/__mocks__/@env.js',
+  },
 };
---
+++ app/tests/__mocks__/@env.js
@@ -0,0 +1,1 @@
+export const IS_TEST_BUILD = 'false';
