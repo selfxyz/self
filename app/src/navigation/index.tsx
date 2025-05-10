00611bf502ae0e4617b5b3798083b6d526addc08
Justin Hernandez <transphorm@gmail.com>
2025-05-10 07:00:32 -0500

    fix route

--- app/src/navigation/index.tsx
+++ app/src/navigation/index.tsx
@@ -36,7 +36,7 @@ export const navigationScreens = {
 };
 
 const AppNavigation = createNativeStackNavigator({
-  initialRouteName: 'LoadingScreen',
+  initialRouteName: 'Splash',
   screenOptions: {
     header: DefaultNavBar,
     navigationBarColor: white,
