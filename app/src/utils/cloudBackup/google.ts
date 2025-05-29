58735ef8daa747ff0928258a3df6e96d3fad743f
Justin Hernandez <transphorm@gmail.com>
2025-05-29 16:10:15 -0500

    setup google web client id

--- app/src/utils/cloudBackup/google.ts
+++ app/src/utils/cloudBackup/google.ts
@@ -1,3 +1,4 @@
+import { GOOGLE_SIGNIN_WEB_CLIENT_ID } from '@env';
 import {
   GoogleSignin,
   isErrorWithCode,
@@ -6,6 +7,8 @@ import {
 
 GoogleSignin.configure({
   scopes: ['https://www.googleapis.com/auth/drive.appdata'],
+  webClientId: GOOGLE_SIGNIN_WEB_CLIENT_ID,
+  offlineAccess: true,
 });
 
 export async function googleSignIn() {
