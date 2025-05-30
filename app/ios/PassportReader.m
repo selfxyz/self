d93b65224a9969d0fc8a8a78fcbff58a31d84883
seshanthS <seshanth@protonmail.com>
2025-05-30 20:26:20 +0530

    feat: update Podfile and include PassportReader.m file

--- app/ios/PassportReader.m
+++ app/ios/PassportReader.m
@@ -10,9 +10,16 @@
 
 @interface RCT_EXTERN_MODULE(PassportReader, NSObject)
 
+RCT_EXTERN_METHOD(configure:(NSString *)token)
+
 RCT_EXTERN_METHOD(scanPassport:(NSString *)passportNumber
                   dateOfBirth:(NSString *)dateOfBirth
                   dateOfExpiry:(NSString *)dateOfExpiry
+                  canNumber:(NSString *)canNumber
+                  useCan:(NSNumber * _Nonnull)useCan
+                  skipPACE:(NSNumber * _Nonnull)skipPACE
+                  skipCA:(NSNumber * _Nonnull)skipCA
+                  extendedMode:(NSNumber * _Nonnull)extendedMode
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 
