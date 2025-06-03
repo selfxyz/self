9037c72ff066568ef65b9ca2eb53ba1cd293cd54
Justin Hernandez <transphorm@gmail.com>
2025-06-02 21:16:49 -0500

    remove unused fonts and fix ios build

--- app/react-native.config.cjs
+++ app/react-native.config.cjs
@@ -3,9 +3,5 @@ module.exports = {
     ios: {},
     android: {},
   },
-  assets: [
-    './src/assets/fonts',
-    '../node_modules/@tamagui/font-inter/otf',
-    '../node_modules/@tamagui/font-silkscreen/files',
-  ],
+  assets: ['./src/assets/fonts'],
 };
