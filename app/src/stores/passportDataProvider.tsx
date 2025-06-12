cbfeb7a937f98d3a56a44b2923e16ff170886ae1
Eric Nakagawa <ericnakagawa@gmail.com>
2025-06-12 09:17:36 -0700

    Remove BSL headers from config files

---
+++ app/src/stores/passportDataProvider.tsx
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: BSL-1.1
+// Copyright (c) 2025 Social Connect Labs, Inc.
+//
+// This file is licensed under the Business Source License 1.1 (BSL-1.1).
+//
+// Use of this software is governed by the Business Source License included in the LICENSE file.
+//
+// As of 2029-06-11, this file will be governed by the Apache License, Version 2.0.
+import React, {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useMemo,
+} from 'react';
+import Keychain from 'react-native-keychain';
+
+import { PassportData } from '../../../common/src/utils/types';
+import { unsafe_getPrivateKey } from '../stores/authProvider';
+import { useAuth } from './authProvider';
+
+// TODO: refactor this as it shouldnt be used directly IMHO
+export async function loadPassportData() {
+  const passportDataCreds = await Keychain.getGenericPassword({
+    service: 'passportData',
+  });
+  return passportDataCreds === false ? false : passportDataCreds.password;
+}
+
+export async function loadPassportDataAndSecret() {
+  const passportData = await loadPassportData();
+  const secret = await unsafe_getPrivateKey();
+  if (!secret || !passportData) {
+    return false;
+  }
+  return JSON.stringify({
+    secret,
+    passportData: JSON.parse(passportData),
+  });
+}
+
+export async function storePassportData(passportData: PassportData) {
+  await Keychain.setGenericPassword(
+    'passportData',
+    JSON.stringify(passportData),
+    { service: 'passportData' },
+  );
+}
+
+export async function clearPassportData() {
+  await Keychain.resetGenericPassword({ service: 'passportData' });
+}
+
+interface PassportProviderProps extends PropsWithChildren {
+  authenticationTimeoutinMs?: number;
+}
+interface IPassportContext {
+  getData: () => Promise<{ signature: string; data: PassportData } | null>;
+  setData: (data: PassportData) => Promise<void>;
+  getPassportDataAndSecret: () => Promise<{
+    data: { passportData: PassportData; secret: string };
+    signature: string;
+  } | null>;
+  clearPassportData: () => Promise<void>;
+}
+
+export const PassportContext = createContext<IPassportContext>({
+  getData: () => Promise.resolve(null),
+  setData: storePassportData,
+  getPassportDataAndSecret: () => Promise.resolve(null),
+  clearPassportData: clearPassportData,
+});
+
+export const PassportProvider = ({ children }: PassportProviderProps) => {
+  const { _getSecurely } = useAuth();
+
+  const getData = useCallback(
+    () => _getSecurely<PassportData>(loadPassportData, str => JSON.parse(str)),
+    [_getSecurely],
+  );
+
+  const getPassportDataAndSecret = useCallback(
+    () =>
+      _getSecurely<{ passportData: PassportData; secret: string }>(
+        loadPassportDataAndSecret,
+        str => JSON.parse(str),
+      ),
+    [_getSecurely],
+  );
+
+  const state: IPassportContext = useMemo(
+    () => ({
+      getData,
+      setData: storePassportData,
+      getPassportDataAndSecret,
+      clearPassportData: clearPassportData,
+    }),
+    [getData, getPassportDataAndSecret],
+  );
+
+  return (
+    <PassportContext.Provider value={state}>
+      {children}
+    </PassportContext.Provider>
+  );
+};
+
+export const usePassport = () => {
+  return useContext(PassportContext);
+};
