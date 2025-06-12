19d55f7a58f3cccc84c2975fd3db6a4b0d4f20ee
turnoffthiscomputer <colin.remi07@gmail.com>
2025-06-12 19:25:43 +0200

    udpate imports and circuits ci

--- circuits/tests/other_circuits/custom_hasher.test.ts
+++ circuits/tests/other_circuits/custom_hasher.test.ts
@@ -12,11 +12,11 @@ const __dirname = path.dirname(__filename);
 
 const defaultIncludePaths = [
   'node_modules',
-  './node_modules/@zk-kit/binary-merkle-root.circom/src',
-  './node_modules/circomlib/circuits',
-  './node_modules/@zk-email/circuits',
-  './node_modules/circom-dl/circuits',
-  './node_modules/@openpassport/zk-email-circuits',
+  'node_modules/@zk-kit/binary-merkle-root.circom/src',
+  'node_modules/circomlib/circuits',
+  'node_modules/@zk-email/circuits',
+  'node_modules/circom-dl/circuits',
+  'node_modules/@openpassport/zk-email-circuits',
 ];
 
 describe('CustomHasher', function () {
