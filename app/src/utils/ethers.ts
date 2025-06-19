8ec994e87d408012dd9a91d4ab05e1fcc19a5182
Justin Hernandez <transphorm@gmail.com>
2025-06-19 11:34:34 -0700

    fix pipeline

--- app/src/utils/ethers.ts
+++ app/src/utils/ethers.ts
@@ -10,17 +10,17 @@ ethers.computeHmac.register((algo, key, data) => {
   return crypto.createHmac(algo, key).update(data).digest();
 });
 
-// @ts-expect-error pbkdf2 is not typed
+// @ts-ignore
 ethers.pbkdf2.register((passwd, salt, iter, keylen, algo) => {
   return crypto.pbkdf2Sync(passwd, salt, iter, keylen, algo);
 });
 
 ethers.sha256.register(data => {
-  // @ts-expect-error sha256 is not typed
+  // @ts-ignore
   return crypto.createHash('sha256').update(data).digest();
 });
 
 ethers.sha512.register(data => {
-  // @ts-expect-error sha512 is not typed
+  // @ts-ignore
   return crypto.createHash('sha512').update(data).digest();
 });
