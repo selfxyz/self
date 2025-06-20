9dcbc3c0cfbc42d6c1544e608a99e040807380fc
Ayman <aymanshaik1015@gmail.com>
2025-06-20 21:22:20 +0530

    chore: rm unnecessary exports

--- sdk/core/index.ts
+++ sdk/core/index.ts
@@ -1,10 +1,7 @@
 import { SelfBackendVerifier } from './src/SelfBackendVerifier.js';
 import { countryCodes } from '@selfxyz/common/constants/constants';
-import { getUniversalLink, SelfAppBuilder } from '@selfxyz/common/utils/appType';
-export type { SelfApp } from '@selfxyz/common/utils/appType';
+import { getUniversalLink } from '@selfxyz/common/utils/appType';
 import { countries } from '@selfxyz/common';
-import { hashEndpointWithScope } from '@selfxyz/common/utils/scope';
-import { getPackedForbiddenCountries } from '@selfxyz/common/utils/contracts/forbiddenCountries';
 import type { AttestationId } from 'src/types/types.js';
 import type { IConfigStorage } from 'src/store/interface.js';
 import { DefaultConfigStore } from 'src/store/DefaultConfigStore.js';
@@ -14,9 +11,6 @@ export {
   countryCodes,
   getUniversalLink,
   countries,
-  hashEndpointWithScope,
-  SelfAppBuilder,
-  getPackedForbiddenCountries,
   AttestationId,
   IConfigStorage,
   DefaultConfigStore
