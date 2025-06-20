59bfc7bb7cdb458d1e46fb5cd049de4df95d6086
Ayman <aymanshaik1015@gmail.com>
2025-06-20 19:51:49 +0530

    feat: consider destination chain in user defined data

--- sdk/core/src/SelfBackendVerifier.ts
+++ sdk/core/src/SelfBackendVerifier.ts
@@ -54,7 +54,7 @@ export class SelfBackendVerifier {
 
     //check if user context hash matches
     const userContextHashInCircuit = publicSignals[discloseIndices[attestationId].userIdentifierIndex];
-    const userContextHash = calculateUserIdentifierHash(userContextData);
+    const userContextHash = calculateUserIdentifierHash(Buffer.from(userContextData, "hex"));
 
     if (userContextHashInCircuit !== userContextHash) {
       issues.push({ type: ConfigMismatch.InvalidUserContextHash, message: 'User context hash does not match with the one in the circuit' });
@@ -72,9 +72,7 @@ export class SelfBackendVerifier {
       if (registryAddress === '0x0000000000000000000000000000000000000000') {
         throw new Error('Registry contract not found');
       }
-      console.log('registryAddress', registryAddress);
       const registryContract = Registry__factory.connect(registryAddress, this.provider);
-      console.log('publicSignals[discloseIndices[attestationId].merkleRootIndex]', publicSignals[discloseIndices[attestationId].merkleRootIndex]);
       const currentRoot = await registryContract.checkIdentityCommitmentRoot(publicSignals[discloseIndices[attestationId].merkleRootIndex]);
       if (!currentRoot) {
         issues.push({ type: ConfigMismatch.InvalidRoot, message: 'Onchain root does not match with the one in the circuit' });
@@ -89,8 +87,8 @@ export class SelfBackendVerifier {
       issues.push({ type: ConfigMismatch.InvalidAttestationId, message: 'Attestation ID does not match with the one in the circuit' });
     }
 
-    const userIdentifier = castToUserIdentifier(BigInt(userContextData.slice(0, 32)), this.userIdentifierType);
-    const userDefinedData = userContextData.slice(32);
+    const userIdentifier = castToUserIdentifier(BigInt(userContextData.slice(64, 128)), this.userIdentifierType);
+    const userDefinedData = userContextData.slice(128);
     const configId = await this.configStorage.getActionId(userIdentifier, userDefinedData);
     const verificationConfig = await this.configStorage.getConfig(configId);
 
@@ -147,7 +145,6 @@ export class SelfBackendVerifier {
     try {
       const verifierAddress = await this.identityVerificationHubContract.discloseVerifier('0x' + attestationId.toString(16).padStart(64, '0'));
       verifierContract = Verifier__factory.connect(verifierAddress, this.provider);
-      console.log('verifierAddress', verifierAddress);
     }catch (error) {
       throw new Error('Verifier contract not found');
     }
@@ -172,88 +169,3 @@ export class SelfBackendVerifier {
     }
   }
 }
-
-(async () => {
-  const allowedIds = new Map<AttestationId, boolean>();
-  allowedIds.set(1, true);
-  allowedIds.set(2, true);
-  const verifier = new SelfBackendVerifier('https://selfxyz.io', 'https://selfxyz.io', true, allowedIds, new DefaultConfigStore({
-    olderThanEnabled: false,
-    olderThan: '18',
-    forbiddenCountriesEnabled: false,
-    forbiddenCountriesListPacked: [
-      '0x414754414154414149414f4741444e414d5341415a44424c41414c41474641',
-      '0x4542524c42425242444742524842534842455a415355415742414d52414752',
-      '0x4e41434d484b5650434e52424c4f424e5442554d424e45425a4c42554d424c',
-      '0x4853454d4559424d5a45575a5455564b4e445453454e4843564943'
-    ],
-    ofacEnabled: [true, true, true]
-  }), 'uuid');
-
-  // await verifier.verify(1, {
-  //   "a": ["20027747457384548915754048546300078359238644739611306732203396740377022269632", "2922252454334934814523912619336295865418779710963506663844841070040892368599"],
-  //   "b": [
-  //     ["18550618272017193017683281599041714361177883341207882150042864009452803287635", "11835322382845835127938425066445170832745075061887634147517477198658857275684"],
-  //     ["9589052941833010873145632477110100396776386222278078408779066519235765125504", "4446299920668248986575867643716084694453281451335382773821734541324874380964"]],
-  //   "c": ["13869518253907120487751576221202706397626340056519243861474381557882676036567", "12690387074286053910397617351082313207917428545513560875234502132363926637665"],
-  //   pubSignals: [
-  //     "0",
-  //     "104974056514807040022473273876258729896969411582978218858119168",
-  //     "1773781688717606310397756023428205332782362187156068670010833396561870848",
-  //     "6818352527182560077653388211384938507686473",
-  //     "0",
-  //     "0",
-  //     "0",
-  //     "8393865390816338643283234268763358234972139160362896016192609318836495598412",
-  //     "1",
-  //     "12042122855944027015224247589071180050575929049407085527795781545283481922120",
-  //     "2",
-  //     "5",
-  //     "0",
-  //     "6",
-  //     "1",
-  //     "5",
-  //     "17359956125106148146828355805271472653597249114301196742546733402427978706344",
-  //     "7420120618403967585712321281997181302561301414016003514649937965499789236588",
-  //     "16836358042995742879630198413873414945978677264752036026400967422611478610995",
-  //     "20547922292487865646889398151624351195094737616498816569824986876525492837134",
-  //     "32484638221990080388773097521078796272"
-  // ]},
-  //   "1234567890"
-  // );
-
-  await verifier.verify(2,
-    {
-      "a": ["12081211863281045519638435814320218331506181778260003189006684934781096301530", "8060260813032756056355953005294591165747245129871184047363247350832615678279"],
-      "b": [
-        ["4347315068897320978820992945813773246810792668754558635019200272564387822377", "15752614222213378619781143491974874432941718433078511349040538945306489946483"],
-        ["21556638492815180585049800142968980107901950444515421443099323631356046322632", "3164272229051284860567978678792807807073207139807100212958388224608073319238"]
-      ],
-      "c": ["5267406348608997194371334102763008174473847620068385896652634372612077418468", "14110837162069354172999846461488917221179735837657004699809342354383332253012"],
-      pubSignals: [
-        "0",
-        "22229121118744123602918790561256186052608",
-        "2154665897585346283029582512769695903067754219393865093262281634727591936",
-        "1",
-        "a6818352527182560077653388211384938507686473",
-        "0",
-        "0",
-        "0",
-        "12816268866992348722154762391251116294707511988403581258809971144485079648161",
-        "2",
-        "4473515946166166681112263978860717654366030938166274938260019677592087803375",
-        "2",
-        "5",
-        "0",
-        "6",
-        "1",
-        "7",
-        "20550865940766091336114076617084411967227963708544788410483208672684333597871",
-        "20607501071671444315195585339157145490348308593668944037177822930025980459166",
-        "13934606664243914063643606771911468856671016933765586820821710153612586828695",
-        "185590350843347269691547707205927446533"
-      ]
-    },
-    "1234567890"
-  );
-})();
--- sdk/core/src/utils/hash.ts
+++ sdk/core/src/utils/hash.ts
@@ -1,8 +1,8 @@
 import { createHash } from "crypto";
 
-export function calculateUserIdentifierHash(userContextData: string): string {
+export function calculateUserIdentifierHash(userContextData: Buffer): string {
   const sha256Hash = createHash("sha256")
-    .update(Buffer.from(userContextData.slice(2), "hex"))
+    .update(userContextData)
     .digest();
   const ripemdHash = createHash("ripemd160").update(sha256Hash).digest();
   return "0x" + ripemdHash.toString("hex").padStart(40, "0");
