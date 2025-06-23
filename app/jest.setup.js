69b7932cc1af910bbda2de65bb7b53bec33774e8
Justin Hernandez <transphorm@gmail.com>
2025-06-23 15:23:49 -0500

    Add BSL license headers to app sources

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -1,3 +1,12 @@
+//  SPDX-License-Identifier: BSL-1.1
+//  Copyright (c) 2025 Social Connect Labs, Inc.
+//
+//  This file is licensed under the Business Source License 1.1 (BSL-1.1).
+//
+//  Use of this software is governed by the Business Source License included in the LICENSE file.
+//
+//  As of 2029-06-11, this file will be governed by the Apache License, Version 2.0.
+
 /* global jest */
 /** @jest-environment jsdom */
 require('react-native-gesture-handler/jestSetup');
