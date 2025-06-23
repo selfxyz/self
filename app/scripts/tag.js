c662c40756e9c1aea60b315b1e63c6a90d720538
Justin Hernandez <transphorm@gmail.com>
2025-06-23 13:38:06 -0700

    fix license reference - https://spdx.org/licenses/BUSL-1.1.html

--- app/scripts/tag.js
+++ app/scripts/tag.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-//  SPDX-License-Identifier: BSL-1.1
+//  SPDX-License-Identifier: BUSL-1.1
 //  Copyright (c) 2025 Social Connect Labs, Inc.
 //
-//  This file is licensed under the Business Source License 1.1 (BSL-1.1).
+//  This file is licensed under the Business Source License 1.1 (BUSL-1.1).
 //
 //  Use of this software is governed by the Business Source License included in the LICENSE file.
 //
