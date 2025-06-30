78d2fdde5a5bd4da2b7410b9225d85e04c40cd1b
Justin Hernandez <transphorm@gmail.com>
2025-06-29 23:13:43 -0700

    ignore adding header to cjs config files

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
-
 module.exports = {
   root: true,
   extends: [
@@ -38,4 +36,12 @@ module.exports = {
     // Override any ESLint rules that conflict with the TypeScript union type formatting
     '@typescript-eslint/indent': 'off',
   },
+  overrides: [
+    {
+      files: ['*.cjs'],
+      rules: {
+        'header/header': 'off',
+      },
+    },
+  ],
 };
