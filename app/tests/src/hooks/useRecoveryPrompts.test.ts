8cda48d6085617e3297b48ef6e826efaa84abb44
Justin Hernandez <transphorm@gmail.com>
2025-06-29 23:04:29 -0700

    ensure there is an extra empty line after the license declaration

--- app/tests/src/hooks/useRecoveryPrompts.test.ts
+++ app/tests/src/hooks/useRecoveryPrompts.test.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
+
 import { renderHook } from '@testing-library/react-native';
 
 import { useModal } from '../../../src/hooks/useModal';
--- app/tests/utils/proving/getPostVerificationRoute.test.ts
+++ app/tests/utils/proving/getPostVerificationRoute.test.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
+
 import { useSettingStore } from '../../../src/stores/settingStore';
 import { getPostVerificationRoute } from '../../../src/utils/proving/provingMachine';
 
