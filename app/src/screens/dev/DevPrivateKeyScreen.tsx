1869c3f1ec82e3ba66fc7cd96c75621cfe927231
nakaz <seanenakamura@gmail.com>
2025-07-20 20:51:59 -0600

    Initial private key screen

---
+++ app/src/screens/dev/DevPrivateKeyScreen.tsx
@@ -0,0 +1,12 @@
+import { Text } from "tamagui";
+
+interface DevPrivateKeyScreen {}
+
+const DevPrivateKeyScreen: React.FC<DevPrivateKeyScreen> = ({}) => {
+  // const navigation = useNavigation();
+  return (
+    <Text>Private Key</Text>
+  )
+}
+
+export default DevPrivateKeyScreen;
