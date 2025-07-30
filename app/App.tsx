01a0279c8b68b526b7d9f5eb40bff486a3e2860a
Justin Hernandez <transphorm@gmail.com>
2025-07-30 12:31:18 -0700

    fix yarn web

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
 
-// CI/CD Pipeline Test - July 29, 2025 - With Permissions Fix
 import 'react-native-get-random-values';
 
 import { Buffer } from 'buffer';
