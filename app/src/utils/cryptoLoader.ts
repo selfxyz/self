2a1cc115ae09249ca5e421960eef06c3bed8a5f1
Justin Hernandez <transphorm@gmail.com>
2025-07-30 16:01:55 -0700

    Revert "add typing to crypto loader"
    This reverts commit d55eec8f4409a9fb6c17680e46e42b4204302d06.

--- app/src/utils/cryptoLoader.ts
+++ app/src/utils/cryptoLoader.ts
@@ -1,12 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
 
-interface CryptoUtils {
-  elliptic: typeof import('elliptic');
-  forge: typeof import('node-forge');
-  ethers: typeof import('ethers');
-}
-
-export const loadCryptoUtils = async (): Promise<CryptoUtils> => {
+export const loadCryptoUtils = async () => {
   const [elliptic, forge, ethers] = await Promise.all([
     import('elliptic'),
     import('node-forge'),
