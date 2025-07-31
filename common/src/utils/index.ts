b208fb3fe4ab0e6ec7917507a4152b272cfabed4
Justin Hernandez <justin.hernandez@self.xyz>
2025-07-31 00:03:44 -0700

    Add tree-shakeable exports

---
+++ common/src/utils/index.ts
@@ -0,0 +1,37 @@
+export {
+  initPassportDataParsing,
+  findStartPubKeyIndex,
+  generateCommitment,
+  generateNullifier,
+} from './passports/passport.js';
+export {
+  genMockIdDoc,
+  generateMockDSC,
+  genMockIdDocAndInitDataParsing,
+  IdDocInput,
+} from './passports/genMockIdDoc.js';
+export { genAndInitMockPassportData } from './passports/genMockPassportData.js';
+export { parseDscCertificateData } from './passports/passport_parsing/parseDscCertificateData.js';
+export { brutforceSignatureAlgorithmDsc } from './passports/passport_parsing/brutForceDscSignature.js';
+export { parseCertificateSimple } from './certificate_parsing/parseCertificateSimple.js';
+export { initElliptic } from './certificate_parsing/elliptic.js';
+export type {
+  CertificateData,
+  PublicKeyDetailsECDSA,
+  PublicKeyDetailsRSA,
+} from './certificate_parsing/dataStructure.js';
+export { getSKIPEM } from './csca.js';
+export { formatMrz } from './passports/format.js';
+export { getCircuitNameFromPassportData } from './circuits/circuitsName.js';
+export * as Hash from './hash.js';
+export { calculateUserIdentifierHash, getSolidityPackedUserContextData } from './hash.js';
+export { getLeafCscaTree, getLeafDscTree, buildSMT } from './trees.js';
+export {
+  generateCircuitInputsDSC,
+  generateCircuitInputsRegister,
+  generateCircuitInputsVCandDisclose,
+} from './circuits/generateInputs.js';
+export type { PassportMetadata } from './passports/passport_parsing/parsePassportData.js';
+export type { UserIdType } from './circuits/uuid.js';
+export * from './appType.js';
+export * from './scope.js';
