53a872cefcea3a4803b7848f0c7f2aa67a29a018
Justin Hernandez <transphorm@gmail.com>
2025-08-06 16:56:09 -0700

    cr feedback and improve tests

--- app/src/components/ErrorBoundary.tsx
+++ app/src/components/ErrorBoundary.tsx
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
 
-import React, { Component, ErrorInfo } from 'react';
+import type { ErrorInfo } from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import * as Sentry from '@sentry/react-native';
 
+import { captureException } from '../Sentry';
 import analytics from '../utils/analytics';
 
 const { flush: flushAnalytics } = analytics();
@@ -29,7 +30,10 @@ class ErrorBoundary extends Component<Props, State> {
   componentDidCatch(error: Error, info: ErrorInfo) {
     // Flush analytics before the app crashes
     flushAnalytics();
-    (Sentry as any)['captureReactException'](error, info);
+    captureException(error, {
+      componentStack: info.componentStack,
+      errorBoundary: true,
+    });
   }
 
   render() {
