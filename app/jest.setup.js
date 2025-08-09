29477e9096b9fc55d8ebcca66127ebb56cb1ac13
Justin Hernandez <transphorm@gmail.com>
2025-08-09 15:42:53 -0700

    fix tests

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -319,6 +319,3 @@ jest.mock('react-native-localize', () => ({
 jest.mock('./src/utils/notifications/notificationService', () =>
   require('./tests/__setup__/notificationServiceMock.js'),
 );
-
-// Import console mocks to reduce test noise
-require('./tests/__setup__/consoleMocks.ts');
