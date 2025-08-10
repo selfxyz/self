268c33c583fa74330692713dee481fc1a5279ef0
Justin Hernandez <transphorm@gmail.com>
2025-08-10 16:16:33 -0700

    new line formatting

--- app/scripts/alias-imports.cjs
+++ app/scripts/alias-imports.cjs
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
+
 const path = require('node:path');
 const { Project, SyntaxKind } = require('ts-morph');
 
