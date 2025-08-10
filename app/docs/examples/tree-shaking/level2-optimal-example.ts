530b4f8fc20ba6a975566534e5b40804f8f59be5
Justin Hernandez <transphorm@gmail.com>
2025-08-10 15:40:42 -0700

    fix license space

--- app/docs/examples/tree-shaking/level2-optimal-example.ts
+++ app/docs/examples/tree-shaking/level2-optimal-example.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
+
 // Level 2 Granular Import Example - Optimal Tree Shaking
 // This demonstrates the new file-based imports for maximum optimization
 
