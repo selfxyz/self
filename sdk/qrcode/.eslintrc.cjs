d1978d254055357d2ed4de7ab3aa8338d93d4045
Justin Hernandez <justin.hernandez@self.xyz>
2025-08-10 17:39:50 -0700

    chore(qrcode): add lint helpers

---
+++ sdk/qrcode/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['simple-import-sort', 'import', 'sort-exports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'sort-exports/sort-exports': ['error', { sortDir: 'asc', ignoreCase: false, sortExportKindFirst: 'type' }],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/newline-after-import': 'error',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
