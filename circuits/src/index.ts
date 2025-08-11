b5540abea85b1a1e635762af2c1bbcebdabc62e9
Justin Hernandez <transphorm@gmail.com>
2025-08-10 21:53:26 -0700

    fixes

--- circuits/src/index.ts
+++ circuits/src/index.ts
@@ -1,22 +1,24 @@
-// Re-export utility functions for generating mock inputs in circuit tests
-export { generateMockRsaPkcs1v1_5Inputs } from '../tests/utils/generateMockInputsInCircuits.js';
-export {
-  generateMockRsaPssInputs,
-  generateMalleableRsaPssInputs,
-} from '../tests/utils/generateMockInputsRsaPss.js';
+// Main circuits package runtime API
+// This package provides circuit definitions and related utilities
 
-// Re-export test case configurations
-export {
-  sigAlgs as registerSigAlgs,
-  fullSigAlgs as registerFullSigAlgs,
-} from '../tests/register/test_cases.js';
-export {
-  sigAlgs as registerIdSigAlgs,
-  fullSigAlgs as registerIdFullSigAlgs,
-} from '../tests/register_id/test_cases.js';
-export { sigAlgs as dscSigAlgs, fullSigAlgs as dscFullSigAlgs } from '../tests/dsc/test_cases.js';
-export {
-  fullAlgorithms as rsaPssFullAlgorithms,
-  sigAlgs as rsaPssSigAlgs,
-  AdditionalCases as rsaPssAdditionalCases,
-} from '../tests/utils/testcase/rsapss.js';
+// Circuit metadata and constants
+export const CIRCUITS_VERSION = '0.0.1';
+
+// Supported signature algorithms
+export const SUPPORTED_SIGNATURE_ALGORITHMS = [
+  'rsa_sha256_65537_2048',
+  'rsa_sha256_65537_3072',
+  'rsa_sha256_65537_4096',
+  'rsa_sha1_65537_2048',
+  'rsa_sha512_65537_4096',
+  'rsapss_sha256_65537_32_3072',
+  'rsapss_sha256_65537_32_4096',
+  'rsapss_sha256_3_32_3072',
+  'rsapss_sha384_65537_48_3072',
+  'rsapss_sha512_65537_64_4096',
+] as const;
+
+export type SupportedSignatureAlgorithm = (typeof SUPPORTED_SIGNATURE_ALGORITHMS)[number];
+
+// Note: Test utilities have been moved to a dedicated './testing' export
+// Import test utilities via: import { ... } from '@selfxyz/circuits/testing'
