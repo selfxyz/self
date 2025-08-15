2346a187ef953ffa2f5068bb03e7b0061a3bf17b
Justin Hernandez <justin.hernandez@self.xyz>
2025-08-14 17:07:27 -0700

    refactor(native-events): validate module and harden tests (#903)

--- packages/mobile-sdk-alpha/src/bridge/nativeEvents.ts
+++ packages/mobile-sdk-alpha/src/bridge/nativeEvents.ts
@@ -1,11 +1,13 @@
 import type { Unsubscribe } from '../types/public';
 
-export type EventHandler = (...args: any[]) => void;
+export type EventHandler = (...args: unknown[]) => void;
 
 export interface NativeEventBridge {
   addListener(moduleName: string, eventName: string, handler: EventHandler): Unsubscribe;
   removeListener(moduleName: string, eventName: string, handler: EventHandler): void;
 }
 
-export const addListener: NativeEventBridge['addListener'] = () => () => {};
+export const addListener: NativeEventBridge['addListener'] = (moduleName, eventName, handler) => () =>
+  removeListener(moduleName, eventName, handler);
+
 export const removeListener: NativeEventBridge['removeListener'] = () => {};
