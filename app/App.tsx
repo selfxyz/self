3c89a2ed0e554a9226fe1b77bd13f0750640c9d3
Justin Hernandez <transphorm@gmail.com>
2025-08-25 09:56:47 -0700

    migrate license headers

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
+// SPDX-FileCopyrightText: 2025 Social Connect Labs, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+// NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
 
 // CI/CD Pipeline Test - July 31, 2025 - With Permissions Fix
 import { Buffer } from 'buffer';
