a1688a289f92cb4115a0d91ff6ce516e856e5328
Justin Hernandez <transphorm@gmail.com>
2025-08-26 15:02:26 -0700

    show generate mock document button

--- app/src/screens/settings/ManageDocumentsScreen.tsx
+++ app/src/screens/settings/ManageDocumentsScreen.tsx
@@ -314,11 +314,9 @@ const ManageDocumentsScreen: React.FC = () => {
             <PrimaryButton onPress={handleScanDocument}>
               Scan New ID Document
             </PrimaryButton>
-            {__DEV__ && (
-              <SecondaryButton onPress={handleGenerateMock}>
-                Generate Mock Document
-              </SecondaryButton>
-            )}
+            <SecondaryButton onPress={handleGenerateMock}>
+              Generate Mock Document
+            </SecondaryButton>
           </ButtonsContainer>
         </YStack>
       </YStack>
