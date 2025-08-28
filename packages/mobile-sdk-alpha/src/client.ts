118b2222efd8497464f88f98dcd56a24d20b8ac0
Leszek Stachowski <leszek.stachowski@self.xyz>
2025-08-28 14:05:12 +0200

    refactor to import type

--- packages/mobile-sdk-alpha/src/client.ts
+++ packages/mobile-sdk-alpha/src/client.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 // NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
 
-import { DocumentCatalog } from '@selfxyz/common/utils/types';
+import type { DocumentCatalog } from '@selfxyz/common/utils/types';
 
 import { defaultConfig } from './config/defaults';
 import { mergeConfig } from './config/merge';
