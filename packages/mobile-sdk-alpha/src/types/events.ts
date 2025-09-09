c69b448bd219aad3183bab70c9306c7366615395
Leszek Stachowski <leszek.stachowski@self.xyz>
2025-09-09 17:03:19 +0200

    lint

--- packages/mobile-sdk-alpha/src/types/events.ts
+++ packages/mobile-sdk-alpha/src/types/events.ts
@@ -3,6 +3,7 @@
 // NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
 
 import { DocumentCategory } from '@selfxyz/common/types';
+
 import type { Progress } from './public';
 
 export enum SdkEvents {
