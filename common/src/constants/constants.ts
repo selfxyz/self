846956d28ef4c481d084830fa816a225fcf016e4
Nesopie <87437291+Nesopie@users.noreply.github.com>
2025-09-12 08:32:45 +0530

    fix: sha256 signed attr tests (#1058)

--- common/src/constants/constants.ts
+++ common/src/constants/constants.ts
@@ -131,6 +131,14 @@ export const MAX_PADDED_ECONTENT_LEN: Partial<Record<(typeof hashAlgos)[number],
   sha512: 896,
 };
 
+export const MAX_PADDED_SIGNED_ATTR_LEN_FOR_TESTS: Record<(typeof hashAlgos)[number], number> = {
+  sha1: 128,
+  sha224: 128,
+  sha256: 256,
+  sha384: 256,
+  sha512: 256,
+};
+
 export const MAX_PADDED_SIGNED_ATTR_LEN: Record<(typeof hashAlgos)[number], number> = {
   sha1: 128,
   sha224: 128,
