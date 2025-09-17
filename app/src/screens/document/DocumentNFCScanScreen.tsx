cd0e5ee96d69e0d1b7f3e11aa911031c21bba906
Justin Hernandez <justin.hernandez@self.xyz>
2025-09-17 16:22:06 -0700

    fix types

--- app/src/screens/document/DocumentNFCScanScreen.tsx
+++ app/src/screens/document/DocumentNFCScanScreen.tsx
@@ -119,7 +119,7 @@ const DocumentNFCScanScreen: React.FC = () => {
     () => ({
       sessionId: sessionIdRef.current,
       platform: Platform.OS as 'ios' | 'android',
-      scanType: route.params?.useCan ? 'can' : 'mrz',
+      scanType: (route.params?.useCan ? 'can' : 'mrz') as 'mrz' | 'can',
     }),
     [route.params?.useCan],
   );
