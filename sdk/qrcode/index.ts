17fcac5dbfa89b5ad5132ba18cb7018ed314e684
Justin Hernandez <justin.hernandez@self.xyz>
2025-09-18 10:51:16 -0700

    fix instructions

--- sdk/qrcode/index.ts
+++ sdk/qrcode/index.ts
@@ -1,5 +1,5 @@
 import type { SelfApp } from '@selfxyz/common';
-import { countries, SelfAppBuilder } from '@selfxyz/common';
+import { countries } from '@selfxyz/common';
 
 import { SelfQRcode, SelfQRcodeWrapper } from './components/SelfQRcode.js';
 import type { WebAppInfo } from './utils/websocket.js';
@@ -8,6 +8,5 @@ export type { SelfApp };
 export type { WebAppInfo };
 
 
-export { SelfAppBuilder };
 export { SelfQRcode, SelfQRcodeWrapper };
 export { countries };
