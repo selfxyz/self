6fd86686e228a56d96cfddaf1f8341237703e282
Justin Hernandez <justin.hernandez@self.xyz>
2025-09-19 17:38:36 -0700

    fix test

--- app/tests/src/navigation.test.ts
+++ app/tests/src/navigation.test.ts
@@ -8,6 +8,8 @@ describe('navigation', () => {
     const listOfScreens = Object.keys(navigationScreens).sort();
     expect(listOfScreens).toEqual([
       'AadhaarUpload',
+      'AadhaarUploadError',
+      'AadhaarUploadSuccess',
       'AccountRecovery',
       'AccountRecoveryChoice',
       'AccountVerifiedSuccess',
