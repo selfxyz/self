dc7232da286c0dcdd1f6797ed6d86ebd70b79bf6
Justin Hernandez <justin.hernandez@self.xyz>
2025-09-26 20:53:16 -0700

    remove file

--- app/src/types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Social Connect Labs, Inc.
-// SPDX-License-Identifier: BUSL-1.1
-// NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
-
-// This file is for global type declarations
-// __DEV__ is already provided by React Native types, so we don't redeclare it
-
-export {};
