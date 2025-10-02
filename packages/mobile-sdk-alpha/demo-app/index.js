68c08a6040a47ed8c9ae750dc9fdcff2ec0e511f
Justin Hernandez <justin.hernandez@self.xyz>
2025-10-02 09:44:12 -0700

    Prune demo app crypto shims

--- packages/mobile-sdk-alpha/demo-app/index.js
+++ packages/mobile-sdk-alpha/demo-app/index.js
@@ -10,15 +10,9 @@
 // eslint-disable-next-line simple-import-sort/imports
 import 'react-native-get-random-values';
 
-import { Buffer } from 'buffer';
 import { AppRegistry } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
-
-import './src/utils/ethers';
-
-// Set global Buffer before any other imports
-global.Buffer = Buffer;
 
 AppRegistry.registerComponent(appName, () => App);
