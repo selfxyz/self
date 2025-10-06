715657c5e3cd5ace67e49f84dc8bb11ba666e7d7
Evi Nova <tranquil_flow@protonmail.com>
2025-10-06 15:33:01 +0200

    refactor: use singular ETHERSCAN_API_KEY in .env
    Etherscan has unified all keys of associated explorers like Celoscan into a singular key rather than different keys for different networks.

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -66,9 +66,9 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: process.env.CELOSCAN_API_KEY as string,
+    apiKey: process.env.ETHERSCAN_API_KEY as string,
     // apiKey: {
-    //   "celo-sepolia": process.env.CELOSCAN_API_KEY as string,
+    //   "celo-sepolia": process.env.ETHERSCAN_API_KEY as string,
     // },
     customChains: [
       {
