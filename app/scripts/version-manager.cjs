dd235cd3f3c4c6d0af185cedaa5e113a7e75fc63
Justin Hernandez <justin.hernandez@self.xyz>
2025-10-09 15:41:38 -0700

    fix script

--- app/scripts/version-manager.cjs
+++ app/scripts/version-manager.cjs
@@ -1,9 +1,8 @@
+#!/usr/bin/env node
+
 // SPDX-FileCopyrightText: 2025 Social Connect Labs, Inc.
 // SPDX-License-Identifier: BUSL-1.1
 // NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
-
-#!/usr/bin/env node
-// SPDX-License-Identifier: BUSL-1.1
 
 /**
  * Centralized Version Manager for Mobile Deployments
