1568a30620b79d8961e8e45c6c2e5a7c09ce36e0
Justin Hernandez <justin.hernandez@self.xyz>
2025-11-11 11:09:31 -0800

    enable buttons

--- app/src/screens/account/recovery/AccountRecoveryChoiceScreen.tsx
+++ app/src/screens/account/recovery/AccountRecoveryChoiceScreen.tsx
@@ -205,7 +205,7 @@ const AccountRecoveryChoiceScreen: React.FC = () => {
           </Description>
 
           <YStack gap="$2.5" width="100%" paddingTop="$6">
-            {/* <PrimaryButton
+            <PrimaryButton
               trackEvent={BackupEvents.CLOUD_BACKUP_STARTED}
               onPress={onRestoreFromTurnkeyPress}
               testID="button-from-turnkey"
@@ -218,7 +218,7 @@ const AccountRecoveryChoiceScreen: React.FC = () => {
             >
               {restoring ? 'Restoring' : 'Restore'} from Turnkey
               {restoring ? '…' : ''}
-            </PrimaryButton> */}
+            </PrimaryButton>
             <PrimaryButton
               trackEvent={BackupEvents.CLOUD_BACKUP_STARTED}
               onPress={onRestoreFromCloudPress}
