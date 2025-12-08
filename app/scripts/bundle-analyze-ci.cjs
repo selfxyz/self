4ec2bcbc83ab5386f6ac5b356adfe1246a2f5a20
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-12-08 15:36:25 +0100

    try this

--- app/scripts/bundle-analyze-ci.cjs
+++ app/scripts/bundle-analyze-ci.cjs
@@ -17,8 +17,8 @@ if (!platform || !['android', 'ios'].includes(platform)) {
 // Bundle size thresholds in MB - easy to update!
 const BUNDLE_THRESHOLDS_MB = {
   // TODO: fix temporary bundle bump
-  ios: 44,
-  android: 44,
+  ios: 45,
+  android: 45,
 };
 
 function formatBytes(bytes) {
