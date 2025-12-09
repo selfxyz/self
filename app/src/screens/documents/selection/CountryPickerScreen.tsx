eb8a2be57ab511ae1c63ecb0517c0c69eb406e75
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-12-09 10:24:01 +0100

    this lint runs twice. once in repo wide lint and once here. so lets just run once to save resources

--- app/src/screens/documents/selection/CountryPickerScreen.tsx
+++ app/src/screens/documents/selection/CountryPickerScreen.tsx
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Social Connect Labs, Inc.
 // SPDX-License-Identifier: BUSL-1.1
 // NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
+
 import type React from 'react';
 
 import SDKCountryPickerScreen from '@selfxyz/mobile-sdk-alpha/onboarding/country-picker-screen';
