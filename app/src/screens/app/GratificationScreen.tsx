d96f2e557b589b77f1b9e80d95ad230a5dbed78b
Justin Hernandez <justin.hernandez@self.xyz>
2026-01-05 15:48:41 -0800

    rename self logo

--- app/src/screens/app/GratificationScreen.tsx
+++ app/src/screens/app/GratificationScreen.tsx
@@ -26,7 +26,7 @@ import {
 } from '@selfxyz/mobile-sdk-alpha/constants/colors';
 import { dinot, dinotBold } from '@selfxyz/mobile-sdk-alpha/constants/fonts';
 
-import LogoWhite from '@/assets/icons/logo_white.svg';
+import SelfLogo from '@/assets/logos/self.svg';
 import GratificationBg from '@/assets/images/gratification_bg.svg';
 import type { RootStackParamList } from '@/navigation';
 
@@ -160,7 +160,7 @@ const GratificationScreen: React.FC = () => {
         >
           {/* Logo icon */}
           <View marginBottom={12} style={styles.logoContainer}>
-            <LogoWhite width={37} height={37} />
+            <SelfLogo width={37} height={37} />
           </View>
 
           {/* Points display */}
