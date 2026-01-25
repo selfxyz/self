afe6616acd2e25631b626bc434d8d787717f0b32
Justin Hernandez <justin.hernandez@self.xyz>
2026-01-25 05:18:00 -0800

    fix mock

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -1251,7 +1251,7 @@ jest.mock('@sumsub/react-native-mobilesdk-module', () => {
   });
 
   const MockSNSMobileSDK = {
-    init: jest.fn().mockImplementation(() => createBuilder()),
+    init: jest.fn().mockImplementation((accessToken, tokenExpirationHandler) => createBuilder()),
   };
 
   return {
