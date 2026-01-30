fcc7bd2ed438d0a9382446056e884a794b6fd68d
Leszek Stachowski <leszek.stachowski@self.xyz>
2026-01-30 11:28:11 +0100

    fix lint

--- app/tests/src/screens/kyc/KycSuccessScreen.test.tsx
+++ app/tests/src/screens/kyc/KycSuccessScreen.test.tsx
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Social Connect Labs, Inc.
 // SPDX-License-Identifier: BUSL-1.1
 // NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
+
 import React from 'react';
 import { v5 as uuidv5 } from 'uuid';
 import { useNavigation } from '@react-navigation/native';
