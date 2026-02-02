797d908096dc1ff3e9bb4fdbc6feb715961db3c8
Justin Hernandez <justin.hernandez@self.xyz>
2026-02-02 11:49:41 -0800

    enable sumsub in mobile sdk

--- packages/mobile-sdk-alpha/src/flows/onboarding/id-selection-screen.tsx
+++ packages/mobile-sdk-alpha/src/flows/onboarding/id-selection-screen.tsx
@@ -11,7 +11,6 @@ import PassportCameraScanIcon from '../../../svgs/icons/passport_camera_scan.svg
 import PlusIcon from '../../../svgs/icons/plus.svg';
 import SelfLogo from '../../../svgs/logo.svg';
 import { BodyText, RoundFlag, View, XStack, YStack } from '../../components';
-import { FeatureFlags } from '../../config/features';
 import { black, blue100, blue600, slate100, slate300, slate400, white } from '../../constants/colors';
 import { advercase, dinot } from '../../constants/fonts';
 import { useSelfClient } from '../../context';
@@ -173,11 +172,9 @@ const IDSelectionScreen: React.FC<IDSelectionScreenProps> = props => {
           <DocumentItem key={docType} docType={docType} onPress={() => onSelectDocumentType(docType)} />
         ))}
         <BodyText style={styles.footerText}>Be sure your document is ready to scan</BodyText>
-        {FeatureFlags.KYC_ENABLED && (
-          <View style={styles.kycContainer}>
-            <DocumentItem docType="kyc" onPress={() => onSelectDocumentType('kyc')} />
-          </View>
-        )}
+        <View style={styles.kycContainer}>
+          <DocumentItem docType="kyc" onPress={() => onSelectDocumentType('kyc')} />
+        </View>
       </YStack>
     </YStack>
   );
