9b243918b7be1f0cf0a54457d22e374b41c5769a
Justin Hernandez <justin.hernandez@self.xyz>
2026-02-02 19:11:29 -0800

    formatting

--- contracts/contracts/verifiers/gcp/Verifier_gcp_jwt.sol
+++ contracts/contracts/verifiers/gcp/Verifier_gcp_jwt.sol
@@ -22,17 +22,17 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Verifier_gcp_jwt {
     // Scalar field size
-    uint256 constant r    = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    uint256 constant r = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
     // Base field size
-    uint256 constant q   = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+    uint256 constant q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
 
     // Verification Key data
-    uint256 constant alphax  = 20491192805390485299153009773594534940189261866228447918068658471970481763042;
-    uint256 constant alphay  = 9383485363053290200918347156157836566562967994039712273449902621266178545958;
-    uint256 constant betax1  = 4252822878758300859123897981450591353533073413197771768651442665752259397132;
-    uint256 constant betax2  = 6375614351688725206403948262868962793625744043794305715222011528459656738731;
-    uint256 constant betay1  = 21847035105528745403288232691147584728191162732299865338377159692350059136679;
-    uint256 constant betay2  = 10505242626370262277552901082094356697409835680220590971873171140371331206856;
+    uint256 constant alphax = 20491192805390485299153009773594534940189261866228447918068658471970481763042;
+    uint256 constant alphay = 9383485363053290200918347156157836566562967994039712273449902621266178545958;
+    uint256 constant betax1 = 4252822878758300859123897981450591353533073413197771768651442665752259397132;
+    uint256 constant betax2 = 6375614351688725206403948262868962793625744043794305715222011528459656738731;
+    uint256 constant betay1 = 21847035105528745403288232691147584728191162732299865338377159692350059136679;
+    uint256 constant betay2 = 10505242626370262277552901082094356697409835680220590971873171140371331206856;
     uint256 constant gammax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
     uint256 constant gammax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
     uint256 constant gammay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
@@ -41,7 +41,6 @@ contract Verifier_gcp_jwt {
     uint256 constant deltax2 = 15902885537441599351050098769394227668772388058868388096316964244217496511682;
     uint256 constant deltay1 = 4195707504005103778106485021796359604414786496137920116128130440872062477216;
     uint256 constant deltay2 = 20513207510859042996645896574478474889840017920990203652675014165180462273668;
-
 
     uint256 constant IC0x = 6972951741762339913362267428319005943611938060812676091174501911982947323821;
     uint256 constant IC0y = 4968121098705797351946375443564156998686441710551907423285338106315203657372;
@@ -106,14 +105,18 @@ contract Verifier_gcp_jwt {
     uint256 constant IC20x = 13029408846315391045768292892963336300734709802776968717851605403617397448869;
     uint256 constant IC20y = 21441391199269244274037661931659719640029973634066921385003370500690694569608;
 
-
     // Memory data
     uint16 constant pVk = 0;
     uint16 constant pPairing = 128;
 
     uint16 constant pLastMem = 896;
 
-    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[20] calldata _pubSignals) public view returns (bool) {
+    function verifyProof(
+        uint[2] calldata _pA,
+        uint[2][2] calldata _pB,
+        uint[2] calldata _pC,
+        uint[20] calldata _pubSignals
+    ) public view returns (bool) {
         assembly {
             function checkField(v) {
                 if iszero(lt(v, r)) {
@@ -197,7 +200,6 @@ contract Verifier_gcp_jwt {
 
                 g1_mulAccC(_pVk, IC20x, IC20y, calldataload(add(pubSignals, 608)))
 
-
                 // -A
                 mstore(_pPairing, calldataload(pA))
                 mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
@@ -222,7 +224,6 @@ contract Verifier_gcp_jwt {
                 mstore(add(_pPairing, 384), mload(add(pMem, pVk)))
                 mstore(add(_pPairing, 416), mload(add(pMem, add(pVk, 32))))
 
-
                 // gamma2
                 mstore(add(_pPairing, 448), gammax1)
                 mstore(add(_pPairing, 480), gammax2)
@@ -238,7 +239,6 @@ contract Verifier_gcp_jwt {
                 mstore(add(_pPairing, 672), deltax2)
                 mstore(add(_pPairing, 704), deltay1)
                 mstore(add(_pPairing, 736), deltay2)
-
 
                 let success := staticcall(sub(gas(), 2000), 8, _pPairing, 768, _pPairing, 0x20)
 
@@ -290,12 +290,11 @@ contract Verifier_gcp_jwt {
 
             checkField(calldataload(add(_pubSignals, 608)))
 
-
             // Validate all evaluations
             let isValid := checkPairing(_pA, _pB, _pC, _pubSignals, pMem)
 
             mstore(0, isValid)
-             return(0, 0x20)
-         }
-     }
- }
+            return(0, 0x20)
+        }
+    }
+}
--- contracts/ignition/modules/hub/updateRegistries.ts
+++ contracts/ignition/modules/hub/updateRegistries.ts
@@ -63,8 +63,6 @@ export default buildModule("UpdateHubRegistries", (m) => {
 
   const hubContract = updateHubRegistries(m, hubAddress, deployedAddresses);
 
-
-
   return {
     hubContract,
   };
