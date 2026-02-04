061dccbc21ffcb901f0b3f68e221338a54d3ea0a
Justin Hernandez <justin.hernandez@self.xyz>
2026-02-04 10:37:51 -0800

    fix test

--- app/tests/src/navigation.test.tsx
+++ app/tests/src/navigation.test.tsx
@@ -107,6 +107,7 @@ describe('navigation', () => {
         'SaveRecoveryPhrase',
         'Settings',
         'ShowRecoveryPhrase',
+        'SocialLoginDemo',
         'Splash',
         'StarfallPushCode',
         'SumsubTest',
