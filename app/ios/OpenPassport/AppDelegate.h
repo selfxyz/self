9347d31ee8037072be45801bfce92d013ed93d43
seshanthS <seshanth@protonmail.com>
2026-02-09 16:21:34 +0530

    add expo 52

--- app/ios/OpenPassport/AppDelegate.h
+++ app/ios/OpenPassport/AppDelegate.h
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1; Copyright (c) 2025 Social Connect Labs, Inc.; Licensed under BUSL-1.1 (see LICENSE); Apache-2.0 from 2029-06-11
 
-#import <RCTAppDelegate.h>
+#import <Expo/Expo.h>
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate>
+@interface AppDelegate : EXAppDelegateWrapper <UNUserNotificationCenterDelegate>
 
 @end
