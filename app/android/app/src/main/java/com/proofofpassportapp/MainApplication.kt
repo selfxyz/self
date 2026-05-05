f9ca2d7d7852dfecf06f5befcc1c4b116f13b1d1
Justin Hernandez <justin.hernandez@self.xyz>
2026-05-05 11:56:06 -0700

    fix android

--- app/android/app/src/main/java/com/proofofpassportapp/MainApplication.kt
+++ app/android/app/src/main/java/com/proofofpassportapp/MainApplication.kt
@@ -10,6 +10,7 @@ import android.content.res.Configuration
 import android.os.Build
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
@@ -17,20 +18,20 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
+import expo.modules.ExpoReactHostFactory
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-      this,
+  private fun createPackages(): MutableList<ReactPackage> =
+      PackageList(this).packages.apply {
+        add(CameraActivityPackage())
+        add(QRCodeScannerPackage())
+        add(BackupPackage())
+      }
+
+  override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): MutableList<ReactPackage> =
-            PackageList(this).packages.apply {
-              add(CameraActivityPackage())
-              add(QRCodeScannerPackage())
-              add(BackupPackage())
-              // add(RNSelfPassportReaderPackage())
-            }
+        override fun getPackages(): MutableList<ReactPackage> = createPackages()
 
         override fun getJSMainModuleName(): String = "index"
 
@@ -39,7 +40,9 @@ class MainApplication : Application(), ReactApplication {
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
-  )
+
+  override val reactHost: ReactHost
+    get() = ExpoReactHostFactory.getDefaultReactHost(this, createPackages())
 
   override fun onCreate() {
     super.onCreate()
