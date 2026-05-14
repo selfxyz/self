ef1e5af502d42f73a36e76cd5bc1426f4a368dcd
Justin Hernandez <justin.hernandez@self.xyz>
2026-05-14 16:01:49 -0700

    format

--- circuits/scripts/link-circom-deps.cjs
+++ circuits/scripts/link-circom-deps.cjs
@@ -19,10 +19,7 @@ const links = [
     '@openpassport/zk-email-circuits',
     path.join(root, 'node_modules', '@openpassport', 'zk-email-circuits'),
   ],
-  [
-    'anon-aadhaar-circuits',
-    path.join(root, 'node_modules', 'anon-aadhaar-circuits'),
-  ],
+  ['anon-aadhaar-circuits', path.join(root, 'node_modules', 'anon-aadhaar-circuits')],
   [
     '@zk-kit/binary-merkle-root.circom',
     path.join(root, 'node_modules', '@zk-kit', 'binary-merkle-root.circom'),
