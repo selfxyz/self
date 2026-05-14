57671bf1c8b38dea5ef64ad2242468f3b07f48e7
Justin Hernandez <justin.hernandez@self.xyz>
2026-05-13 18:33:01 -0700

    fix

--- app/react-native.config.cjs
+++ app/react-native.config.cjs
@@ -6,13 +6,6 @@ const dependencies = {
   '@selfxyz/mobile-sdk-alpha': { platforms: { android: null, ios: null } },
 };
 
-// Disable Didit SDK autolinking during E2E testing to avoid build issues
-if (process.env.E2E_TESTING === '1') {
-  dependencies['@didit-protocol/sdk-react-native'] = {
-    platforms: { android: null, ios: null },
-  };
-}
-
 module.exports = {
   project: { ios: {}, android: {} },
   dependencies,
