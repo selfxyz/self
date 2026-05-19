aaef2673e6a2f4f09a3584cf9b339114ff9626ed
Justin Hernandez <justin.hernandez@self.xyz>
2026-05-19 14:02:09 -0700

    fix

--- app/scripts/bundle-analyze-ci.cjs
+++ app/scripts/bundle-analyze-ci.cjs
@@ -6,7 +6,7 @@
 const { execSync } = require('child_process');
 const { existsSync, statSync, unlinkSync } = require('fs');
 const os = require('os');
-const { join } = require('path');
+const { join, resolve } = require('path');
 
 const platform = process.argv[2];
 if (!platform || !['android', 'ios'].includes(platform)) {
@@ -61,6 +61,8 @@ function checkBundleSize(bundleSize, targetPlatform) {
 const tmpDir = os.tmpdir();
 const bundleFile = join(tmpDir, `${platform}.bundle`);
 const sourcemapFile = join(tmpDir, `${platform}.bundle.map`);
+const appRoot = resolve(__dirname, '..');
+const entryFile = join(appRoot, 'index.js');
 
 console.log(`🔨 Generating ${platform} bundle using Metro...`);
 
@@ -69,7 +71,7 @@ try {
     `npx react-native bundle ` +
       `--platform ${platform} ` +
       `--dev false ` +
-      `--entry-file index.js ` +
+      `--entry-file ${entryFile} ` +
       `--bundle-output ${bundleFile} ` +
       `--sourcemap-output ${sourcemapFile} ` +
       `--minify false ` +
