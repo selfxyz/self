84fe9fa235331ef3ee7de03f933508d01f3648bf
Justin Hernandez <justin.hernandez@self.xyz>
2026-05-28 10:25:02 -0700

    formatting

--- app/src/specs/PassportOCRViewNativeComponent.ts
+++ app/src/specs/PassportOCRViewNativeComponent.ts
@@ -3,7 +3,8 @@
 // NOTE: Converts to Apache-2.0 on 2029-06-11 per LICENSE.
 
 import type * as React from 'react';
-import { CodegenTypes, codegenNativeComponent, type ViewProps } from 'react-native';
+import type { CodegenTypes, ViewProps } from 'react-native';
+import { codegenNativeComponent } from 'react-native';
 
 type PassportReadEvent = Readonly<{
   data: string;
